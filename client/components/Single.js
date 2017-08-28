6d17090130062d54f07f0342ba477c9e9d5a2b1e
Luke Fiji <luke.fiji@gmail.com>
2017-08-27 22:35:46 -0700

    Finished video 16 - implemented REMOVE_COMMENT

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Photo from './Photo';
+import Comments from './Comments';
+// import comments
 
 const Single = React.createClass({
   render() {
-    return <div className="single-photo">I'm the single photo</div>;
+    const { posts, params, comments } = this.props;
+
+    // Index of the post
+    const i = posts.findIndex((post) => post.code === params.postId);
+    const post = posts[i];
+
+    const postComments = comments[params.postId] || [];
+
+    // Get us the post
+    return (
+      <div className="single-photo">
+        <Photo i={i} post={post} {...this.props} />
+        <Comments postComments={postComments} {...this.props} />
+      </div>
+    );
   }
 });
 
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,37 @@
+// Handling { postId: [comments] }
+function postComments(state = [], action) {
+  switch (action.type) {
+    case 'ADD_COMMENT':
+      // Return the new state with the new comment added
+      return [
+        ...state,
+        {
+          user: action.author,
+          text: action.comment
+        }
+      ];
+      break;
+    case 'REMOVE_COMMENT':
+      // Return new state without the deleted comment
+      return [...state.slice(0, action.i), ...state.slice(action.i + 1)];
+      break;
+    default:
+      return state;
+  }
+  return state;
+}
+
+// Reducer composition
 function comments(state = [], action) {
-  console.log(state, action);
+  // Check if reducer has to run
+  if (typeof action.postId !== 'undefined') {
+    return {
+      // Take current state
+      ...state,
+      // Overwrite post with a new one - pass it a sub-piece of comments state
+      [action.postId]: postComments(state[action.postId], action)
+    };
+  }
   return state;
 }
 
